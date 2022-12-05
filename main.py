dd790b0b13d3b059317958651700117225b2e2d2
samstarke <sam@starke.gg>
2022-12-05 14:24:43 -0600

    Update main.py

--- main.py
+++ main.py
@@ -18,13 +18,13 @@ async def on_ready():
 async def hello(interaction: discord.Interaction):
     await interaction.response.send_message(f"Hey {interaction.user.mention}! This is a slash command!", ephemeral=True)
 
-@bot.tree.command(name="hey")
+@bot.tree.command(name="say")
 @app_commands.describe(arg = "What should I say?")
 async def say(interaction: discord.Interaction, arg: str):
     await interaction.response.send_message(f"{interaction.user.name} said: `{arg}`")
 
 # create a slash command to increment a counter by 1 when using the command count
-@bot.tree.command(name="yeayea")
+@bot.tree.command(name="count")
 async def count(interaction: discord.Interaction):
     # get the counter value from the interaction
     counter = interaction.data.get("options", {}).get("counter", 0)
