f5ca9ffe7ee32a422603ef309cf509d3d7d85f8b
samstarke <sam@starke.gg>
2023-02-22 15:09:54 -0600

    approve reject

--- main.py
+++ main.py
@@ -19,59 +19,73 @@ async def on_ready():
 async def submit(interaction: discord.Interaction, name: str, members: str, description: str, link: str):
     # Split members string into a list
     members_list = members.split(",")
-    # Create a formatted string with each member on a separate line
     members_str = "\n".join(members_list)
     
-    embed = discord.Embed(title=f"{name} Project Submission", description=f"`{description}`", color=0x38DE4C)
+    embed = discord.Embed(title=f"{name} Project Submission", description=f"`{description}`", color=0x7289da)
     embed.add_field(name="Team Members", value=members_str, inline=False)
     embed.add_field(name="GitHub Link", value=f"[{link}]({link})", inline=False)
     embed.set_footer(text="Thanks for submitting! Administrators will get to your submission shortly.")
     
-    # Send the embed to the interaction channel
     await interaction.response.send_message(embed=embed)
-    
-    # Send a notification to the submissions channel
     channel = bot.get_channel(1049411277434388493)
+    
+    message = f"{name} has posted their project submission in <#{interaction.channel_id}>.\nPlease check the description for approval.\nMembers: {members_str}"
+    
     role = channel.guild.get_role(1049411275735695402)
+    
+    await channel.send(f"{message}")
 
-    embed = discord.Embed(title=f"{name} Project Submission", description=f"{name} has posted their project submission in <#{interaction.channel_id}>. Please check the description for approval.", color=0x7289da)
+@bot.tree.command(name='approve')
+@commands.has_role('Administrator')
+@app_commands.describe(name="Provide the team name.")
+async def approve(interaction: discord.Interaction, name: str):
+    check_channel_id = 1049411277434388493
+    check_channel = bot.get_channel(check_channel_id)
+    approval_channel_id = 1078004744137756703
+    approval_channel = bot.get_channel(approval_channel_id)
 
-    await channel.send(role.mention, embed=embed)
+    async for message in check_channel.history():
+        if message.author.bot and name in message.content:
+            members_str = message.content.split('\n')[1]
+            members = [member.strip() for member in members_str.split(',')]
+            mentions = message.mentions
+            mention_str = ' '.join(f'<@{m}>' for m in mentions)
 
+            approval_message = f"{mention_str}"
+            approval_embed = discord.Embed(title=f"{name} Project Approved", description=f"Your project has been approved by <@{interaction.user.id}>!", color=0x84eb80)
+            await approval_channel.send(content=approval_message, embed=approval_embed)
+            await interaction.response.send_message(f"Approved {name} project submission.")
+            return
 
-# @bot.tree.command(name='approve')
-# @commands.has_role('Administrator')
-# @app_commands.describe(name="Provide the team name.")
-# async def approve(interaction: discord.Interaction, name: str):
-#     print("Inside approve function.")
-#     submissions_channel = bot.get_channel(1078018927877169204)
-#     async for embed in submissions_channel.history():
-#         if embed.author == bot.user:
-#             continue
-#         if len(embed.embeds) > 0:
-#             embed = embed.embeds[0]
-#             print(f"Checking embed with title: {embed.title}")
-#             if name in embed.title:
-#                 print("Found matching submission.")
-#                 team_members = embed.fields[0].value.replace('**Team Members:** ', '').split('\n')
-#                 team_mention = ' '.join([f"<@{member.split(' ')[-1]}>" for member in team_members])
-#                 approved_embed = discord.Embed(title=f"`{name} Project Approved!`", color=0x38DE4C)
-#                 approved_embed.add_field(name="Team Members", value=embed.fields[0].value, inline=False)
-#                 approved_embed.add_field(name="Project Description", value=embed.fields[1].value, inline=False)
-#                     continue
-#                 approved_embed.add_field(name="Project Link", value=embed.fields[2].value, inline=False)
-#                 approved_embed.set_footer(text="Congratulations! Your project has been approved.")
-#                 await interaction.response.send_message(embed=approved_embed)
-#                 message = f"Project submission by {name} has been approved! {team_mention}"
-#                 approval_channel = bot.get_channel(1078004744137756703)
-#                 await approval_channel.send(message)
-#                 return
-#     print("No matching submission found.")
-#     await interaction.response.send_message(f"No project submission found with the name {name}")
+    await interaction.response.send_message(f"Could not find submission for {name}.")
+
+@bot.tree.command(name='reject')
+@commands.has_role('Administrator')
+@app_commands.describe(name="Provide the team name and a reason for the rejection.")
+async def reject_submission(interaction: discord.Interaction, name: str, reason: str):
+    check_channel_id = 1049411277434388493
+    check_channel = bot.get_channel(check_channel_id)
+    rejection_channel_id = 1078004744137756703
+    rejection_channel = bot.get_channel(rejection_channel_id)
+
+    async for message in check_channel.history():
+        if message.author.bot and name in message.content:
+            members_str = message.content.split('\n')[1]
+            members = [member.strip() for member in members_str.split(',')]
+            mentions = message.mentions
+            mention_str = ' '.join(f'<@{m}>' for m in mentions)
+
+            rejection_message = f"{mention_str}"
+            rejection_embed = discord.Embed(title=f"{name} Project Rejected", description=f"Your project has been rejected by <@{interaction.user.id}> for the following reason:\n\n{reason}\n\nPlease see the feedback and feel free to submit a revised version.", color=0xE84E4E)
+            await rejection_channel.send(content=rejection_message, embed=rejection_embed)
+            await interaction.response.send_message(f"Rejected {name} project submission with reason: {reason}.")
+            return
+
+    await interaction.response.send_message(f"Could not find submission for {name}.")
+
 
 @bot.tree.command(name="ping")
 async def ping(interaction: discord.Interaction):
-    # send a message to the user
     await interaction.response.send_message("Pong!")
 
 bot.run(TOKEN)
