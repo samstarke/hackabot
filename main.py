9f74b7f2787a9e07d3cf46c5e98bb5abb65c3ea1
samstarke <sam@starke.gg>
2023-03-04 16:20:49 -0600

    Update main.py

--- main.py
+++ main.py
@@ -27,62 +27,17 @@ async def submit(interaction: discord.Interaction, name: str, members: str, desc
     embed.set_footer(text="Thanks for submitting! Administrators will get to your submission shortly.")
     
     await interaction.response.send_message(embed=embed)
-    channel = bot.get_channel(1049411277434388493)
-    
+    channel = bot.get_channel(1077647771059888241)
     message = f"{name} has posted their project submission in <#{interaction.channel_id}>.\nPlease check the description for approval.\nMembers: {members_str}"
-    
     role = channel.guild.get_role(1049411275735695402)
-    
-    await channel.send(f"{message}")
+    await channel.send(f"{role} {message}")
 
-@bot.tree.command(name='approve')
-@commands.has_role('Administrator')
-@app_commands.describe(name="Provide the team name.")
-async def approve(interaction: discord.Interaction, name: str):
-    check_channel_id = 1049411277434388493
-    check_channel = bot.get_channel(check_channel_id)
-    approval_channel_id = 1078004744137756703
-    approval_channel = bot.get_channel(approval_channel_id)
-
-    async for message in check_channel.history():
-        if message.author.bot and name in message.content:
-            members_str = message.content.split('\n')[1]
-            members = [member.strip() for member in members_str.split(',')]
-            mentions = message.mentions
-            mention_str = ' '.join(f'<@{m}>' for m in mentions)
-
-            approval_message = f"{mention_str}"
-            approval_embed = discord.Embed(title=f"{name} Project Approved", description=f"Your project has been approved by <@{interaction.user.id}>!", color=0x84eb80)
-            await approval_channel.send(content=approval_message, embed=approval_embed)
-            await interaction.response.send_message(f"Approved {name} project submission.")
-            return
-
-    await interaction.response.send_message(f"Could not find submission for {name}.")
-
-@bot.tree.command(name='reject')
-@commands.has_role('Administrator')
-@app_commands.describe(name="Provide the team name and a reason for the rejection.")
-async def reject_submission(interaction: discord.Interaction, name: str, reason: str):
-    check_channel_id = 1049411277434388493
-    check_channel = bot.get_channel(check_channel_id)
-    rejection_channel_id = 1078004744137756703
-    rejection_channel = bot.get_channel(rejection_channel_id)
-
-    async for message in check_channel.history():
-        if message.author.bot and name in message.content:
-            members_str = message.content.split('\n')[1]
-            members = [member.strip() for member in members_str.split(',')]
-            mentions = message.mentions
-            mention_str = ' '.join(f'<@{m}>' for m in mentions)
-
-            rejection_message = f"{mention_str}"
-            rejection_embed = discord.Embed(title=f"{name} Project Rejected", description=f"Your project has been rejected by <@{interaction.user.id}> for the following reason:\n\n{reason}\n\nPlease see the feedback and feel free to submit a revised version.", color=0xE84E4E)
-            await rejection_channel.send(content=rejection_message, embed=rejection_embed)
-            await interaction.response.send_message(f"Rejected {name} project submission with reason: {reason}.")
-            return
-
-    await interaction.response.send_message(f"Could not find submission for {name}.")
-
+# create a help command that gives embeds of the usefulness of each command and how to use them
+@bot.tree.command(name="help")
+async def help(interaction: discord.Interaction):
+    embed = discord.Embed(title="Help", description="This is a list of commands you can use in the bot.", color=0x7289da)
+    embed.add_field(name="/submit", value="Use this command to submit your project. You will be prompted to provide the team name, list of members, a brief description of your project, and a GitHub link to your project.", inline=False)
+    await interaction.response.send_message(embed=embed)
 
 @bot.tree.command(name="ping")
 async def ping(interaction: discord.Interaction):
